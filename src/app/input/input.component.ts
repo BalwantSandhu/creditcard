0a3f10248541afd77b738e242ff2271a1519e5c1
BalwantSandhu <bsingh3@unb.ca>
2026-01-08 03:24:23 -0400

    Added input component which can be reused to create input elements

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NgIf, NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-input',
+  imports: [ReactiveFormsModule, NgIf, NgClass],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent {
+  @Input() control: FormControl;
+  @Input() label: string;
+
+  showErrors(){
+    const {dirty, touched, errors} = this.control;
+    return dirty && touched && errors;
+  }
+}
